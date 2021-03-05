cfd023d7e095ae9a564390605050ca057d161774
Jeroen Zwart <jeroen.zwart@live.com>
2021-03-05 18:19:40 +0100

    added token authentication

--- functions/index.js
+++ functions/index.js
@@ -40,11 +40,40 @@ app.get('/screams', (req, res) => {
     .catch(err => console.error(err))
 })
 
-app.post('/scream', (req, res) => {
+// the Middleware FBauth verifies the authentication token of the user. It can be user to validate the user before access is provided within a given path
+const FBauth = (res, req, next) => { 
+    let idToken;
+    if(req.heades.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idtoken = res.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({ error: 'Unauthorized' })
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken)
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        }) //hold the data thats inside the token
+        .then(() => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token ', err);
+            return res.status(403).json(err);
+        })
+}
+
+app.post('/scream', FBauth, (req, res) => {
 
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
