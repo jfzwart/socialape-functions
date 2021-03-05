7f4f944b08ceed5cbdaa3d82deb89e0d318e501e
Jeroen Zwart <jeroen.zwart@live.com>
2021-03-05 18:29:21 +0100

    fixed some bugs

--- functions/index.js
+++ functions/index.js
@@ -41,10 +41,10 @@ app.get('/screams', (req, res) => {
 })
 
 // the Middleware FBauth verifies the authentication token of the user. It can be user to validate the user before access is provided within a given path
-const FBauth = (res, req, next) => { 
+const FBauth = (req, res, next) => { 
     let idToken;
-    if(req.heades.authorization && req.headers.authorization.startsWith('Bearer ')){
-        idtoken = res.headers.authorization.split('Bearer ')[1];
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
     } else {
         console.error('No token found')
         return res.status(403).json({ error: 'Unauthorized' })
@@ -59,7 +59,7 @@ const FBauth = (res, req, next) => {
                 .limit(1)
                 .get();
         }) //hold the data thats inside the token
-        .then(() => {
+        .then((data) => {
             req.user.handle = data.docs[0].data().handle;
             return next();
         })
