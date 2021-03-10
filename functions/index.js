7ec36ae6d15624e357ac0ff9ef2872260a8a5144
Jeroen Zwart <jeroen.zwart@live.com>
2021-03-10 11:25:44 +0100

    added notification functions on like and comment

--- functions/index.js
+++ functions/index.js
@@ -44,3 +44,68 @@ app.post('/user', FBAuth, addUserDetails)
 app.get('/user', FBAuth, getAuthenticatedUser)
 
 exports.api = functions.region('europe-west1').https.onRequest(app); // will automatically transform into multiple routes
+
+// create a notification on liking a scream
+
+exports.createNotificationOnLike = region('europe-west1').firestore.document(`/likes/${id}`)
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+            .then(doc => {
+                if(doc.exist){
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'like',
+                        read: false,
+                        screamId: doc.id,
+                    });
+                }
+            })
+            .then(()=>{
+                return;
+            })
+            .catch(err => {
+                console.error(err);
+                return;
+            })
+    })
+
+exports.deleteNotificationOnUnlike = region('europe-west1').firestore.document(`/likes/${id}`)
+    .onDelete((snapshot) => {
+        db.doc(`/notifications/${snapshot.id}`)
+        .delete()
+        .then(()=>{
+            return;
+        })
+        .catch(err => {
+            console.error(err);
+            return;
+        })
+    })
+
+
+
+exports.createNotificationOnComment = region('europe-west1').firestore.document(`/comment/${id}`)
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+            .then(doc => {
+                if(doc.exist){
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'comment',
+                        read: false,
+                        screamId: doc.id,
+                    });
+                }
+            })
+            .then(()=>{
+                return;
+            })
+            .catch(err => {
+                console.error(err);
+                return;
+            })
+    })
